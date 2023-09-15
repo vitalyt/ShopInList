b26a4f42264c10b951744219bb68421ac3a5b345
Vitalii Todorovych <vitalyii.todorovych@vista.com>
2023-09-15 22:58:47 +0300

    Initiate WatchKit App target

---
+++ ShopInListWatch Watch App/ShopInListWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopInListWatchApp.swift
+//  ShopInListWatch Watch App
+//
+//  Created by Vitalii Todorovych on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopInListWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
